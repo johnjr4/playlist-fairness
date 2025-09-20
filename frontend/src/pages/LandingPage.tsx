432ca9633f76c120e7b5399a88c719a30b617890
John Randolph <jnhoj2002@icloud.com>
2025-09-20 02:14:01 -0500

    Landing page button and init styling

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,7 +1,25 @@
+import { useNavigate } from "react-router";
+import Button from "../components/ui/Button";
+import { useAuth } from "../utils/AuthContext";
+
 function LandingPage() {
+    const navigate = useNavigate();
+    const { user } = useAuth();
+
+    function onClick() {
+        if (user) {
+            navigate('/u');
+        } else {
+            navigate('/auth/login');
+        }
+    }
+
     return (
         <div className="flex flex-col justify-center items-center h-full">
-            It's the landing page!
+            <h1 className="font-bold text-7xl p-6">Spotifair</h1>
+            <Button onClick={onClick}>
+                Get Started
+            </Button>
         </div>
     )
 }
