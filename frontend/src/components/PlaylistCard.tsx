b4bf688ea4beedd34b684a91ca01a861da1a3f80
John Randolph <jnhoj2002@icloud.com>
2025-09-25 01:00:36 -0500

    Home page hover effects

--- frontend/src/components/PlaylistCard.tsx
+++ frontend/src/components/PlaylistCard.tsx
@@ -2,14 +2,20 @@ import * as Public from 'spotifair'
 import CoverArt from './ui/CoverArt';
 import { useNavigate } from 'react-router';
 
-function PlaylistCard({ playlist, className }: { playlist: Public.Playlist, className?: string }) {
+interface PlaylistCardProps {
+    playlist: Public.Playlist;
+    className?: string;
+    wrapperClassName?: string;
+}
+
+function PlaylistCard({ playlist, className, wrapperClassName }: PlaylistCardProps) {
     const navigate = useNavigate();
     function handleClick() {
         navigate(`/u/${playlist.id}`);
     }
     return (
-        <li>
-            <button className={`p-3 rounded-sm ${className}`} onClick={() => handleClick()}>
+        <li className={`${wrapperClassName} rounded-sm`}>
+            <button className={`p-3 rounded-sm ${className} cursor-pointer`} onClick={() => handleClick()}>
                 <CoverArt
                     coverUrl={playlist.coverUrl}
                     alt={`${playlist.name} cover art`}
