3946e6968125a71d22290b5a4f4eb42d6c3a0c29
John Randolph <jnhoj2002@icloud.com>
2025-09-26 00:29:20 -0500

    Playlist sync indicator

--- frontend/src/components/PlaylistCard.tsx
+++ frontend/src/components/PlaylistCard.tsx
@@ -1,6 +1,7 @@
 import * as Public from 'spotifair'
 import CoverArt from './ui/CoverArt';
 import { useNavigate } from 'react-router';
+import { FaSyncAlt } from 'react-icons/fa';
 
 interface PlaylistCardProps {
     playlist: Public.Playlist;
@@ -14,13 +15,18 @@ function PlaylistCard({ playlist, className }: PlaylistCardProps) {
     }
     return (
         <li className={`${className} rounded-sm relative p-3.5`}>
-            <button className={`w-full flex flex-col gap-2 rounded-sm cursor-pointer`} onClick={() => handleClick()}>
+            <button className={`w-full flex flex-col items-center gap-2 rounded-sm cursor-pointer`} onClick={() => handleClick()}>
                 <CoverArt
                     coverUrl={playlist.coverUrl}
                     alt={`${playlist.name} cover art`}
                     size='w-full'
                 />
-                <p className='h-5 text-md/tight line-clamp-1 truncate'>{playlist.name}</p>
+                <p className='md:h-5 w-[80%] text-xs/tight sm:text-sm/tight md:text-md/tight line-clamp-1 truncate'>{playlist.name}</p>
+                <FaSyncAlt
+                    color={playlist.syncEnabled ? 'var(--color-textPrimary)' : 'var(--color-background-100)'}
+                    // text size controls the SVG size (apparently)
+                    className='absolute bottom-2.5 right-2 md:bottom-3 md:right-3 text-[8.5px] md:text-[11px] lg:text-[13px]'
+                />
             </button>
         </li>
     )
