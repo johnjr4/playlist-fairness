d3a08f0f5b5511ae15925c548d307dee0ff0f950
John Randolph <jnhoj2002@icloud.com>
2025-10-02 20:45:51 -0500

    Fix CoverArt, adjust svg cover colors

--- frontend/src/components/ui/CoverArt.tsx
+++ frontend/src/components/ui/CoverArt.tsx
@@ -11,24 +11,29 @@ interface CoverArtProps {
 }
 
 function CoverArt({ coverUrl, alt = 'Cover Image', size = 'w-48', className }: CoverArtProps) {
-    const [status, setStatus] = useState<'loading' | 'error' | 'loaded'>(!coverUrl ? 'error' : 'loading');
-
+    const [status, setStatus] = useState<'loading' | 'error' | 'loaded'>('loading');
     return (
         <div className={`aspect-square ${size} overflow-hidden shadow-md shadow-background-400 ${className}`}>
-            {status === 'loading' && <img src={loadingCoverUrl} alt={alt} className="w-full h-full" />}
-            {status !== 'error'
-                ? <img
-                    src={coverUrl!} // Can assert because of initial status state
-                    alt={alt}
-                    className="w-full h-full object-cover"
-                    onLoad={() => setStatus('loaded')}
-                    onError={() => setStatus('error')}
-                />
-                : <img
+            {(coverUrl === null || coverUrl === '' || status === 'error')
+                ?
+                <img
                     src={errorCoverUrl} // Can assert because of initial status state
                     alt={alt}
                     className="w-full h-full object-cover"
                 />
+                :
+                (
+                    <>
+                        {status === 'loading' && <img src={loadingCoverUrl} alt={alt} className="w-full h-full" />}
+                        <img
+                            src={coverUrl!} // Can assert because of initial status state
+                            alt={alt}
+                            className="w-full h-full object-cover"
+                            onLoad={() => setStatus('loaded')}
+                            onError={() => setStatus('error')}
+                        />
+                    </>
+                )
             }
         </div>
     )
