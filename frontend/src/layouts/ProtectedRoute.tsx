e210e16802559b81846205e18bee5b500d43728e
John Randolph <jnhoj2002@icloud.com>
2025-10-13 18:26:05 -0500

    Fix ProtectedRoute loading styling

--- frontend/src/layouts/ProtectedRoute.tsx
+++ frontend/src/layouts/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router";
 import { useAuth } from "../utils/AuthContext";
 import { useEffect, type ReactElement } from "react";
+import gradientClasses from '../styling/gradient.module.css';
 
 function ProtectedRoute({ children }: { children: ReactElement }) {
     const { user, loading } = useAuth();
@@ -12,7 +13,11 @@ function ProtectedRoute({ children }: { children: ReactElement }) {
             navigate('/');
         }
     }, [user, loading, navigate]);
-    if (loading) return <div>Loading...</div>
+    if (true) return (
+        <div className={`${gradientClasses['bgGradient']} w-full h-full text-textPrimary flex justify-center items-center`}>
+            Loading...
+        </div>
+    )
 
     return user ? children : null;
 }
